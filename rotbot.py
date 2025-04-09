111faec1367c93d9b0a1da40934944549e04e56f
Haylia <39031808+Haylia@users.noreply.github.com>
2025-04-09 14:36:57 +0100

    dmcheck and mass haul new commands

--- rotbot.py
+++ rotbot.py
@@ -26,6 +26,8 @@ help_command = commands.DefaultHelpCommand(
 client = commands.Bot(command_prefix = PREFIX, intents = intents, case_insensitive = True, activity = bot_activity, help_command=help_command)
 timenow = datetime.now()
 
+userwithunreaddms = []
+
 instaloaderL = instaloader.Instaloader()
 
 def get_real_instagram_url(share_url):
@@ -91,59 +93,169 @@ async def dmhaul(ctx, user: discord.User=None):
     # iterate through messages since the last bot message in the channel, newest first
     # await ctx.send(f"Getting terrible instagram memes from {user.display_name}'s dms")
     sentany = False
-    async for message in user.dm_channel.history(oldest_first=False):
-        if message.author == client.user:
-            # this is the bot message, break
-            if sentany == False:
-                await ctx.send(f"There's no new memes in {user.display_name}'s dms")
-            break
-        else:
-            # expecting instagram link, if not, skip
-            # check if the message is a link
-            if message.content.startswith("https://www.instagram.com/") or message.content.startswith("https://instagram.com/"):
-                reallink = get_real_instagram_url(message.content)
-                # check if the link is a reel or post
-                if "/p/" in reallink:
-                    # get the link
-                    link = reallink
-                    # get the id of the post/
-                    try:
-                        post = instaloader.Post.from_shortcode(instaloaderL.context, link.split("/")[-2])
-                        # download the post
-                        diddownload = instaloaderL.download_pic(filename=post.shortcode, url=post.url, mtime=post.date_utc)
-                        if diddownload:
-                            # send the file to the ctx channel
-                            await ctx.send(file=discord.File(f"{post.shortcode}.jpg"))
-                            # delete the file
-                            os.remove(f"{post.shortcode}.jpg")
-                            sentany = True
-                    except Exception as e:
-                        print(e)
-                elif "/reel/" in reallink:
-                    try:
+    async with ctx.typing():
+        async for message in user.dm_channel.history(oldest_first=False):
+            if message.author == client.user:
+                # this is the bot message, break
+                break
+            else:
+                # expecting instagram link, if not, skip
+                # check if the message is a link
+                if message.content.startswith("https://www.instagram.com/") or message.content.startswith("https://instagram.com/"):
+                    reallink = get_real_instagram_url(message.content)
+                    # check if the link is a reel or post
+                    if "/p/" in reallink:
                         # get the link
                         link = reallink
-                        # get the id of the reel
-                        post = instaloader.Post.from_shortcode(instaloaderL.context, link.split("/")[-2])
-                        video_url = post.video_url
-                        filename = instaloaderL.format_filename(post, target=post.owner_username)
-                        diddownload = instaloaderL.download_pic(filename=filename, url=video_url, mtime=post.date_utc)
-                        if diddownload:
-                            filename = filename + ".mp4"
-                            # send the file to the ctx channel
-                            await ctx.send(file=discord.File(filename))
-                            # delete the file
-                            os.remove(filename)
-                            sentany = True
-                    except Exception as e:
-                        print(e)
-                else:
-                    # this is not a reel or post, skip
+                        # get the id of the post/
+                        try:
+                            post = instaloader.Post.from_shortcode(instaloaderL.context, link.split("/")[-2])
+                            # download the post
+                            diddownload = instaloaderL.download_pic(filename=post.shortcode, url=post.url, mtime=post.date_utc)
+                            if diddownload:
+                                # send the file to the ctx channel
+                                await ctx.send(file=discord.File(f"{post.shortcode}.jpg"))
+                                # delete the file
+                                os.remove(f"{post.shortcode}.jpg")
+                                sentany = True
+                        except Exception as e:
+                            print(e)
+                    elif "/reel/" in reallink:
+                        try:
+                            # get the link
+                            link = reallink
+                            # get the id of the reel
+                            post = instaloader.Post.from_shortcode(instaloaderL.context, link.split("/")[-2])
+                            video_url = post.video_url
+                            filename = instaloaderL.format_filename(post, target=post.owner_username)
+                            diddownload = instaloaderL.download_pic(filename=filename, url=video_url, mtime=post.date_utc)
+                            if diddownload:
+                                filename = filename + ".mp4"
+                                # send the file to the ctx channel
+                                await ctx.send(file=discord.File(filename))
+                                # delete the file
+                                os.remove(filename)
+                                sentany = True
+                        except Exception as e:
+                            print(e)
+                    else:
+                        # this is not a reel or post, skip
+                        continue
+                    
+        # send a message in the user's dm channel (will be used to mark messages that have already been scanned)
+        if sentany:
+            await user.dm_channel.send(f"Sent to here")
+
+@client.command()
+async def dmcheck(ctx):
+    global userwithunreaddms
+    # check all users in guild for new dms since the last bot message, output the count per member
+    memberstomessages = dict()
+    # get all members in the guild
+    # iterate through all members
+    async with ctx.typing():
+        for user in client.users:
+            try:
+                messages = 0
+                if user == client.user:
+                    # skip the bot
+                    print("skipping self")
                     continue
-            
-    # send a message in the user's dm channel (will be used to mark messages that have already been scanned)
-    if sentany:
-        await user.dm_channel.send(f"Sent to here")
+                if not user.dm_channel:
+                    await user.create_dm()
+                    memberstomessages[user] = messages
+                # check if the user has sent any messages since the last bot message
+                async for message in user.dm_channel.history(oldest_first=False):
+                    if message.author == client.user:
+                        # this is the bot message, break
+                        break
+                    else:
+                        # this is not the bot message, increment the count
+                        messages += 1
+                memberstomessages[user] = messages
+                print(f"{user.display_name} has {messages} messages since the last bot message")
+            except:
+                # this user can't be dmed
+                continue
+        # sort the dictionary by value
+        memberstomessages = dict(sorted(memberstomessages.items(), key=lambda item: item[1], reverse=True))
+        # create an embed
+        embed = Embed(title="DM Check", description="", color=discord.Color.blue())
+        # iterate through the dictionary and add fields to the embed
+        for user, messages in memberstomessages.items():
+            if messages > 0:
+                embed.add_field(name=user.display_name, value=messages, inline=False)
+                userwithunreaddms.append(user.id)
+        # send the embed to the ctx channel
+        await ctx.send(embed=embed)
+    
+@client.command()
+async def dmhaulall(ctx):
+    global userwithunreaddms
+    for userid in userwithunreaddms:
+        user = client.get_user(userid)
+        
+        # if user.dm_channel is None:
+        #     await user.create_dm()
+            # await user.dm_channel.send(f"Hello {user.name}, there's been a meme haul request but you've not sent me anything. RIP")
+
+        # try to get the message history
+        # iterate through messages since the last bot message in the channel, newest first
+        # await ctx.send(f"Getting terrible instagram memes from {user.display_name}'s dms")
+        sentany = False
+        async with ctx.typing():
+            async for message in user.dm_channel.history(oldest_first=False):
+                if message.author == client.user:
+                    # this is the bot message, break
+                    break
+                else:
+                    # expecting instagram link, if not, skip
+                    # check if the message is a link
+                    if message.content.startswith("https://www.instagram.com/") or message.content.startswith("https://instagram.com/"):
+                        reallink = get_real_instagram_url(message.content)
+                        # check if the link is a reel or post
+                        if "/p/" in reallink:
+                            # get the link
+                            link = reallink
+                            # get the id of the post/
+                            try:
+                                post = instaloader.Post.from_shortcode(instaloaderL.context, link.split("/")[-2])
+                                # download the post
+                                diddownload = instaloaderL.download_pic(filename=post.shortcode, url=post.url, mtime=post.date_utc)
+                                if diddownload:
+                                    # send the file to the ctx channel
+                                    await ctx.send(file=discord.File(f"{post.shortcode}.jpg"))
+                                    # delete the file
+                                    os.remove(f"{post.shortcode}.jpg")
+                                    sentany = True
+                            except Exception as e:
+                                print(e)
+                        elif "/reel/" in reallink:
+                            try:
+                                # get the link
+                                link = reallink
+                                # get the id of the reel
+                                post = instaloader.Post.from_shortcode(instaloaderL.context, link.split("/")[-2])
+                                video_url = post.video_url
+                                filename = instaloaderL.format_filename(post, target=post.owner_username)
+                                diddownload = instaloaderL.download_pic(filename=filename, url=video_url, mtime=post.date_utc)
+                                if diddownload:
+                                    filename = filename + ".mp4"
+                                    # send the file to the ctx channel
+                                    await ctx.send(file=discord.File(filename))
+                                    # delete the file
+                                    os.remove(filename)
+                                    sentany = True
+                            except Exception as e:
+                                print(e)
+                        else:
+                            # this is not a reel or post, skip
+                            continue
+                        
+            # send a message in the user's dm channel (will be used to mark messages that have already been scanned)
+            if sentany or userid in userwithunreaddms:
+                await user.dm_channel.send(f"Sent to here")
+                userwithunreaddms.remove(userid)
 
 
 client.run(TOKEN)
